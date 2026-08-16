a7f2c0f9cf6cfaf5818135eeecf6ee3e6022e6b9
Devendra Pandey <25032tjf97devendrakumar@viva-technology.org>
2026-08-16 14:39:17 +0530

    Implement Caller JFrame with button action

--- Caller.java
+++ Caller.java
@@ -1,1 +1,52 @@
 
+
+public class Caller extends javax.swing.JFrame {
+    
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Caller.class.getName());
+    public Caller() {
+        initComponents();
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("Open CheckBox");
+        jButton1.addActionListener(this::jButton1ActionPerformed);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(148, 148, 148)
+                .addComponent(jButton1)
+                .addContainerGap(168, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(98, 98, 98)
+                .addComponent(jButton1)
+                .addContainerGap(179, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>                        
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        new RadioCheck();
+        dispose();
+    }                                        
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(() -> new Caller().setVisible(true));
+    }
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JButton jButton1;
+    // End of variables declaration                   
+}
